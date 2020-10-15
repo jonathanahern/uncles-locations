565dab11f312fe67a150e0edcc49ef3f3d3a2b97
jonathanahern <modestfool@gmail.com>
2020-10-15 13:04:31 -0400

    Try again with orig

--- app/controllers/app_proxy_controller.rb
+++ app/controllers/app_proxy_controller.rb
@@ -10,70 +10,70 @@ class AppProxyController < ApplicationController
   def index
 
 
-    # locations = {
-    #   "79065841797" => "Bellevue Crossroads Mall&https://www.unclesgames.com/pages/crossroads-mall",
-    #   "78964031621" =>"Redmond Towncenter&https://www.unclesgames.com/pages/redmond-town-center",
-    #   "77614874757" =>"Spokane Valley Mall&https://www.unclesgames.com/pages/spokane-valley-uncles",
-    #   "77411942533" =>"Downtown Spokane&https://www.unclesgames.com/pages/downtown-spokane-uncles"
-    # }
+    locations = {
+      "79065841797" => "Bellevue Crossroads Mall&https://www.unclesgames.com/pages/crossroads-mall",
+      "78964031621" =>"Redmond Towncenter&https://www.unclesgames.com/pages/redmond-town-center",
+      "77614874757" =>"Spokane Valley Mall&https://www.unclesgames.com/pages/spokane-valley-uncles",
+      "77411942533" =>"Downtown Spokane&https://www.unclesgames.com/pages/downtown-spokane-uncles"
+    }
 
-    # locationsInventory = []
+    locationsInventory = []
   
-    # ShopifyAPI::Session.setup(api_key: ShopifyApp.configuration.api_key, secret: ShopifyApp.configuration.secret)
-    # uncToken = Shop.first.shopify_token;
-    # session = ShopifyAPI::Session.new(domain: params[:shop] , token: uncToken, api_version: "2020-04")
-    # ShopifyAPI::Base.activate_session(session)
-    # ShopifyAPI::Base.api_version = ShopifyApp.configuration.api_version
+    ShopifyAPI::Session.setup(api_key: ShopifyApp.configuration.api_key, secret: ShopifyApp.configuration.secret)
+    uncToken = Shop.first.shopify_token;
+    session = ShopifyAPI::Session.new(domain: params[:shop] , token: uncToken, api_version: "2020-04")
+    ShopifyAPI::Base.activate_session(session)
+    ShopifyAPI::Base.api_version = ShopifyApp.configuration.api_version
  
-    # client = ShopifyAPI::GraphQL.client
+    client = ShopifyAPI::GraphQL.client
 
-    # query = ShopifyAPI::GraphQL.client.parse <<-'GRAPHQL'
-    #   query ($id: ID!) {
-    #     productVariant(id: $id) {
-    #       id
-    #       title
-    #       inventoryItem {
-    #         id
-    #       }
-    #     }
-    #   }
-    # GRAPHQL
+    query = ShopifyAPI::GraphQL.client.parse <<-'GRAPHQL'
+      query ($id: ID!) {
+        productVariant(id: $id) {
+          id
+          title
+          inventoryItem {
+            id
+          }
+        }
+      }
+    GRAPHQL
 
-    # varID = params[:varID]
-    # result = ShopifyAPI::GraphQL.client.query( query, variables: { id: "gid://shopify/ProductVariant/#{varID}" })
-    # invID = result.data.product_variant.inventory_item.id.split('/').pop
+    varID = params[:varID]
+    result = ShopifyAPI::GraphQL.client.query( query, variables: { id: "gid://shopify/ProductVariant/#{varID}" })
+    invID = result.data.product_variant.inventory_item.id.split('/').pop
       
-    # queryLoc = ShopifyAPI::GraphQL.client.parse <<-'GRAPHQL'
-    #   query($id: ID!) {
-    #   inventoryItem(id: $id) {
-    #     id
-    #     inventoryLevels (first:4) {
-    #       edges {
-    #         node {
-    #           id
-    #           available
-    #         }
-    #       }
-    #     }
-    #   }
-    #   }
-    # GRAPHQL
+    queryLoc = ShopifyAPI::GraphQL.client.parse <<-'GRAPHQL'
+      query($id: ID!) {
+      inventoryItem(id: $id) {
+        id
+        inventoryLevels (first:4) {
+          edges {
+            node {
+              id
+              available
+            }
+          }
+        }
+      }
+      }
+    GRAPHQL
 
-    # resultLoc = ShopifyAPI::GraphQL.client.query( queryLoc, variables: { id: "gid://shopify/InventoryItem/#{invID}" })
-    # locArr = resultLoc.data.inventory_item.inventory_levels.edges
+    resultLoc = ShopifyAPI::GraphQL.client.query( queryLoc, variables: { id: "gid://shopify/InventoryItem/#{invID}" })
+    locArr = resultLoc.data.inventory_item.inventory_levels.edges
 
-    # locArr.each do |ele|
-    #   locNum = ele.node.id.split("?")[0].split("/").pop;
-    #   num = ele.node.available;
-    #   num = "0" if num.to_i < 0;
-    #   if (locations.has_key?(locNum))
-    #     locName = locations[locNum];
-    #     locationsInventory << [locName,num]
-    #   end
-    # end
+    locArr.each do |ele|
+      locNum = ele.node.id.split("?")[0].split("/").pop;
+      num = ele.node.available;
+      num = "0" if num.to_i < 0;
+      if (locations.has_key?(locNum))
+        locName = locations[locNum];
+        locationsInventory << [locName,num]
+      end
+    end
 
-    # render json: {"Inventory": locationsInventory }
-    render json: {"Inventory": "coming back to you" }
+    render json: {"Inventory": locationsInventory }
+    # render json: {"Inventory": "coming back to you" }
 
   end
 
